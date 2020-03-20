556eb37d9195d74a5c597329629d4845af5cbf67
nsturjo <35952129+nsturjo@users.noreply.github.com>
2020-03-20 12:12:10 +0100

    Update MSR_Ass_3.py
    updated

--- MSR_Ass_3.py
+++ MSR_Ass_3.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 18 14:28:06 2020
 
-@author: nstur
-"""
 from datetime import datetime
 from github import Github
 import pandas as pd
@@ -11,42 +7,42 @@ import requests
 import time
 import json
 
+
 g = Github("nsturjo@gmail.com", "***")
 df = pd.read_csv(r'H:\WS 19-20\Mining Software Repositories\Assignment2\sample.csv')
 
 gh_session = requests.Session()
 gh_session.auth = ("nsturjo@gmail.com", "***")
-#github_api = "https://api.github.com"
-#gh_session = requests.Session()
-#gh_session.auth = ("nsturjo@gmail.com", "***")
 
-#gh_session.get(url = url)
+
+
+f = open('out.txt','w')
 
 all_repo = df.repository
 
+# iteration over all the repos in csv file
 for item in all_repo:
-    repo = g.get_repo(item)
+    try:
+        repo = g.get_repo(item)
+    except:
+        print("Error fetching {} repository".format(item), file=f)
     path = "pom.xml"
     owner = item.split("/")[0]
     repo_name = item.split("/")[1]   
     url = "https://api.github.com/repos/{}/{}/commits?path={}".format(owner, repo_name, path)
-    
-#    repo = g.get_repo(item)
-    
-    
-    
+    # getting all the commits over a specific file path from git api
     r = gh_session.get(url)
     if(r.status_code == 403):
-        print("Rate Limit exceeded! Try later.")
+        print("Rate Limit exceeded! Try later.", file=f)
         break
     if(r.status_code == 404):
-        print("File {} doesn't exist.".format(path))
+        print(path + "file doesn't exist.", file=f)
         continue
-    time.sleep(1)
+    time.sleep(0.73)
     
     json_response = json.loads(r.text)
     
-    
+    # variables for extracting data from the raw pom.xml file
     maven_model_start_tag_txt = "<modelVersion>"
     maven_model_end_tag_txt = "</modelVersion>"
     antlr_version_start_tag_txt = "<antlr.version>"
@@ -54,34 +50,34 @@ for item in all_repo:
     antlr4_version_start_tag_txt = "<antlr4.version>"
     antlr4_version_end_tag_txt = "</antlr4.version>"
     
-    
+    # output data frame declaration
     df_output = pd.DataFrame(columns =  ["Repo", "File_name", "Download_url", "Commit","Time", "Antlr_version"])
     
     
     file_name = "pom.xml"
-    for i, data in enumerate(json_response):
+    for data in json_response:  # iterating over each commit over specific pom.xml file
         
         sha = data["sha"]
         raw_date = data['commit']['author']['date']
         commit_time = datetime.strptime(raw_date, '%Y-%m-%dT%H:%M:%SZ')
         
-        
         try:
-            contents = repo.get_contents(file_name, ref=sha)
+            contents = repo.get_contents(file_name, ref=sha)  # using PyGithub method to fetch the download_url of the pom.xml file
         except:
-            print("{} not found for commit {}".format(file_name,sha))
-        
+            print(file_name + " not found for commit " + sha, file=f)
+        # getting the file from git api
         raw_file = requests.get(contents.download_url)
         raw_file = raw_file.text
-        time.sleep(1)
+        time.sleep(0.73)
         
         maven_versio_start_index = raw_file.find(maven_model_start_tag_txt) + len(maven_model_start_tag_txt)
         maven_version_end_index = raw_file.find(maven_model_end_tag_txt)
         
         maven_version_no = raw_file[maven_versio_start_index:maven_version_end_index]
 
+        # extracting antlr version from raw_file using string operation
         antlr_version = "N/A"
-        if raw_file.find(antlr_version_start_tag_txt) != -1:
+        if raw_file.find(antlr_version_start_tag_txt) != -1:   
             antlr_version_start_index = raw_file.find(antlr_version_start_tag_txt) + len(antlr_version_start_tag_txt)
             antlr_version_end_index = raw_file.find(antlr_version_end_tag_txt)
             antlr_version = raw_file[antlr_version_start_index:antlr_version_end_index]
@@ -99,9 +95,9 @@ for item in all_repo:
                 cropped_file = raw_file[groupid_tag_index:(groupid_tag_index + raw_file[groupid_tag_index:].find("</dependency>"))]
                 antlr_version = cropped_file[cropped_file.find("<version>") + 9 : cropped_file.find("</version>")]
             
-        print_preview = df_output.loc[i] = [item,file_name,contents.download_url,sha,commit_time,antlr_version]
-        print(print_preview)   
+        # printing output in text file and adding individual revision data into global output dataframe
+        print_preview = [item,file_name,contents.download_url,sha,commit_time,antlr_version]
+        df_output.loc[len(df_output)] = print_preview
+        print(print_preview, file=f)
             
-            
-
-#print(all_repo)
+    
